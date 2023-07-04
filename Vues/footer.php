09b69554ae38a4f613e2622e7c827a9df563da91
Vahiniaina <randrianjatovov@gmail.com>
2023-07-04 10:47:05 +0300

    todaycommit

--- Vues/footer.php
+++ Vues/footer.php
@@ -2,41 +2,42 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-6 col-12  text-center">
-                <ul class="learn"> <h4>A savoir:</h4>
-                    <li>A propos</li>
+                <ul class="learn"> <h4>Help and support</h4>
+                    <li>About us</li>
                     <li>FAQ</li>
-                    <li>Aide</li>
-                    <li>Signaler un problem</li>
-                    <li>Conditions et reglements </li>
-                    <li>Parametre et confidentialité</li>
+                    <li>Help</li>
+                    <li>Report a problem</li>
+                    <li>Terms of service</li>
+                    <li>Setting and privacy</li>
                 </ul>
             </div>
             <div class="col-lg-3 col-md-6 col-12 text-center">
-                 <h4>Communauté:</h4>
-                 <a class="btn btn-outline-primary" href="/Sciences/Vues/signup.php">Souscrire</a>
+                 <h4>Community</h4>
+                 <a class="btn btn-outline-primary" href="/Forum/Vues/signup.php">Sign up</a><br>
+                 <a class="btn btn-outline-primary" href="/Forum/Vues/signin.php">Sign in</a>
             </div>
             <div class="col-lg-3 col-md-6 col-12 text-center">  
-                 <h4>Nous joindre:</h4>  
-                 <div><i class="fa fa-phone" class="p-2">+261325045819</i></div>
+                 <h4>Follow us on</h4>  
+                 <div><i class="fa fa-phone" class="p-2">+261388623981</i></div>
                 <ul class="followus" class="list"> <h4>Suivez nous:</h4>
                     <li>
                          <a href="https://mobile.facebook.com/?_rdc=1&_rdr" title="RANDRIANJATOVO Vahiniaina" target="_blank">
-                                <img src="assets/images/facebook.png" width="30" height="30" alt="Icone facebook">
+                                <img src="../assets/images/facebook.png" width="30" height="30" alt="Icone facebook">
                          </a>
                     </li>
                     <li>
                         <a href="https://www.instagram.com/" title="RANDRIANJATOVO Vahiniaina" target="_blank">
-                            <img src="assets/images/instagram.png" width="30" height="30" alt="Icone instagram" >
+                            <img src="../assets/images/instagram.png" width="30" height="30" alt="Icone instagram" >
                         </a>
                     </li>
                </ul>
                  
             </div>
             <div class="col-lg-3 col-md-6 col-12  text-center">
-                <h4>Récherche</h4>
-                <form class="d-flex" method="post" action="/web-dev/Controllers/search.php">
+                <h4>Search</h4>
+                <form class="d-flex" method="post" action="../Controllers/search.php">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="keyword">
-                            <input class="btn btn-outline-primary" type="submit" value="Recherche">
+                            <input class="btn btn-outline-primary" type="submit" value="Search">
                 </form> 
             </div>
         </div>  
--- index.php
+++ index.php
@@ -1,19 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<!--head-->
-<head>
-    <title>Acceuille</title>
-    <meta charset="utf-8" >
-    <meta name="viewport" content="width=device-width" />
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="fontawesome/css/all.css">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script src="bootstrap/js/jquery-3.5.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <!--Footer-->
-    <h1>Hello World</h1>
-    <?php include(dirname(__FILE__).'/Vues/footer.php'); ?>
-</body>
-</html>
+<?php 
+    header("Location: Vues/home.php");
